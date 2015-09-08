cbdd896aa745c92f456e566ef391a703934f39c8
Victor do Val <Superman@Victors-MacBook-Pro.local>
2015-09-08 18:46:22 -0500

    Initial Commit

---
+++ SlideSnap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideSnap
+//
+//  Created by Victor do Val on 9/8/15.
+//  Copyright (c) 2015 Victor do Val. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
